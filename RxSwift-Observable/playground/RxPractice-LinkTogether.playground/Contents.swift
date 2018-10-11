936ffad2a030a230513adaccf4440305f6e6968f
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-10-12 02:00:17 +0900

    impl map

--- RxSwift-Observable/playground/RxPractice-LinkTogether.playground/Contents.swift
+++ RxSwift-Observable/playground/RxPractice-LinkTogether.playground/Contents.swift
@@ -2,3 +2,22 @@ import UIKit
 import Foundation
 import RxSwift
 
+extension Observable {
+    public func myMap<T> (converter: @escaping (E) throws -> T) -> Observable<T> {
+        return Observable<T>.create { observer in
+            let disposable = self.subscribe(
+                onNext: {
+                    do {
+                        try observer.onNext(converter($0))
+                    } catch let e {
+                        observer.onError(e)
+                    }
+            }, onError: {
+                observer.onError($0)
+            }, onCompleted: {
+                observer.onCompleted()
+            })
+            return disposable
+        }
+    }
+}
